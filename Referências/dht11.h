a65cc2889007451ec632974ed7a7d954c0dd140a
Aline Rosa <42355665+alinerrosa@users.noreply.github.com>
2021-04-20 11:55:54 -0300

    Create dht11.h

---
+++ Referências/dht11.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+DHT11.h
+*******************************************************************************/
+#ifndef DHT11_H
+#define DHT11_H
+
+#include <pigpio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <vector>
+
+int read_dht11_dat()
+
+#endif // DHT11_H
