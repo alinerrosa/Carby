22e1a3e45d89416026ef63f8e344a25c2be15196
Sofia Consolmagno <31675868+consolmagno@users.noreply.github.com>
2021-04-21 00:37:24 -0300

    Update sensor_dht11.cpp

--- Código-fonte/sensor_dht11.cpp
+++ Código-fonte/sensor_dht11.cpp
@@ -1,57 +1,117 @@
-#include <wiringPi.h>
+#include <pigpio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <vector>
+#include <stdbool.h>
+#include <fstream>
+#include <limits.h>
+#include <string>
 
 using namespace std;
-#define MAXTIMINGS 85
-#define DHTPIN 7
-vector<int> dht11_dat(5, 0);
+#define DHTPIN 4
+bool ok = true;
 
-void read_dht11_dat(){
-	int laststate = HIGH;
-	int counter = 0;
-	int j = 0, i;
-	dht11_dat.assign(5,0);
+static unsigned cleanupPin = UINT_MAX;
+static bool verbose = false;
 
-	pinMode(DHTPIN, OUTPUT);
-	digitalWrite(DHTPIN, LOW);
-	delay(20);
-	digitalWrite(DHTPIN, HIGH);
-	delayMicroseconds(40);
-	pinMode( DHTPIN, INPUT );
-
-	for (i = 0; i < MAXTIMINGS; i++){
-		counter = 0;
-		while (digitalRead(DHTPIN) == laststate){
-			counter++;
-			delayMicroseconds(1);
-			if (counter == 0xFF)
-				break;
-		}
-		laststate = digitalRead(DHTPIN);
-
-		if (counter == 0xFF)
-			break;
-
-		if ((i >= 4) && (i % 2 == 0)){
-			dht11_dat[j / 8] <<= 1;
-			if (counter > 50)
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	}
-	if ((j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
-		cout << "Umidade = " << dht11_dat[0] << "." << dht11_dat[1] << " % Temperatura = " << dht11_dat[2] << "." << dht11_dat[3] << " *C" << '\n';
+int read_dht11(unsigned pin) {
+    gpioSetMode(pin, PI_OUTPUT);
+    gpioDelay(19 * 1000);
+    gpioSetMode(pin, PI_INPUT);
+    return 0;
 }
 
-int main(){
-	if (wiringPiSetup() == -1)
-		exit(1);
-	while (1){
-		read_dht11_dat();
-		delay(3000);
-	}
-	return(0);
+static void cleanup(void) {
+    if (verbose) {
+      fprintf(stderr,"... cleanup()\n");
+    }
+    if (cleanupPin != UINT_MAX) {
+      gpioSetPullUpDown( cleanupPin, PI_PUD_OFF);
+    }
+    gpioTerminate();
+}
+
+enum pulse_state { PS_IDLE = 0, PS_PREAMBLE_STARTED, PS_DIGITS };
+
+static void pulse_reader(int gpio, int level, uint32_t tick) {
+    ofstream database; 
+    database.open ("database.csv", std::ofstream::out | std::ofstream::app);
+    if(ok){
+      database << "Temperatura;Umidade\n";
+    }
+    ok = false;
+    static uint32_t lastTick = 0;
+    static enum pulse_state state = PS_IDLE;
+    static uint64_t accum = 0;
+    static int count = 0;
+    uint32_t len = tick - lastTick;
+    lastTick = tick;
+
+    switch (state) {
+      case PS_IDLE:
+        if (level == 1 && len > 70 && len < 95) state = PS_PREAMBLE_STARTED;
+        else state = PS_IDLE;
+        break;
+      case PS_PREAMBLE_STARTED:
+  if (level == 0 && len > 70 && len < 95) {
+      state = PS_DIGITS;
+      accum = 0;
+      count= 0;
+  } else state = PS_IDLE;
+    break;
+      case PS_DIGITS:
+  if (level == 1 && len >= 35 && len <= 65);
+  else if (level == 0 && len >= 15 && len <= 35) { 
+    accum <<= 1;
+    count++; 
+  }
+  else if (level == 0 && len >= 60 && len <= 80) { 
+    accum = (accum << 1) + 1; 
+    count++; 
+  }
+  else state = PS_IDLE;
+
+  if (count == 40) {
+      state = PS_IDLE;
+      uint8_t parity = (accum & 0xff);
+      uint8_t tempLow = ((accum>>8) & 0xff);
+      uint8_t tempHigh = ((accum>>16) & 0xff);
+      uint8_t humLow = ((accum>>24) & 0xff);
+      uint8_t humHigh = ((accum>>32) & 0xff);
+      uint8_t sum = tempLow + tempHigh + humLow + humHigh;
+      bool valid = (parity == sum);
+      
+      if (valid) {
+        printf("{\"Temperature\": %d,%d, \"Humidity\": %d,%d}\n", tempHigh, tempLow, humHigh, humLow);
+        string temp = to_string(tempHigh) + ',' + to_string(tempLow);
+        string humi = to_string(humHigh) + ',' + to_string(humLow);
+        database << temp << ';' << humi << '\n';
+      }
+    }
+    break;
+  }
+  if (verbose) {
+    printf("pulse %c %4uµS state=%d digits=%d\n", (level==0 ? 'H' : (level==1?'L':'W') ), len, state, count);
+  }
+}
+
+int main( int argc, char **argv) {
+    unsigned pin = DHTPIN;
+    // database << "Temperature,Humidity\n"; 
+    if (gpioInitialise() == PI_INIT_FAILED) {
+      fprintf(stderr, "failed to initialize GPIO\n");
+      exit(EXIT_FAILURE);
+    }
+    atexit(cleanup);
+    gpioSetMode( pin, PI_INPUT);
+    gpioSetPullUpDown( pin, PI_PUD_UP);
+    gpioWrite( pin, 0); 
+    cleanupPin = pin; 
+    gpioSetWatchdog(pin, 50);
+    while (1) {
+      gpioSetAlertFunc( pin, pulse_reader);
+      read_dht11(pin);
+      gpioDelay( 100*1000); // wait 1/10 sec between tries
+    }
+    exit(2);
+    return 0;
 }
